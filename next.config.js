8f9b30cdeffd426a920712a5af1715ace7bcca6b
Dyno <dyno@taldeMacBook-Air.local>
2025-12-12 13:38:05 +0800

    配置 GitHub Pages 部署和 GitHub Actions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/demo-C-platform' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/demo-C-platform' : '',
   images: {
+    unoptimized: true,
     domains: ['picsum.photos', 'images.unsplash.com'],
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
